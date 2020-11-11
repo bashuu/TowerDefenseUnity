8b53dd5c9ddf69a210c4cb56700f5357adf9a03c
Sumyakhuu <sumyahuu@gmail.com>
2020-11-11 21:58:29 +0800

    added enemy, dirTrigger,

---
+++ Assets/Scripts/enemy.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemy : MonoBehaviour
+{
+    public Vector2 movement;
+    public float speed = 10f;
+    void Start()
+    {
+        movement = new Vector2(1.0f, 0.0f);
+    }
+        
+    private void Update()
+    {
+        transform.Translate(movement * Time.deltaTime * speed);
+        //Debug.Log("testing");
+    }
+}
---
+++ Assets/Scripts/triggerDir.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class triggerDir : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D coll)
+    {
+
+        if (coll.tag == "Enemy")
+        {
+            Debug.Log("GameObject2 collided with " + coll.name);
+        }
+    }
+}
