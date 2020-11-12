0c91d662b0a3e2728059e23ec59942acb541fa7f
Sumyakhuu <sumyahuu@gmail.com>
2020-11-12 14:23:27 +0800

    added tower, towerManager, mouseinput
    now can create tower in game

---
+++ Assets/Scripts/mouseInput.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mouseInput : MonoBehaviour
+{
+    private Vector3 mousePoss;
+    public GameObject towerManager;
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            mousePoss = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            towerManager.GetComponent<towerManager>().createTower(mousePoss);
+            Debug.Log(mousePoss);
+        }
+    }
+}
