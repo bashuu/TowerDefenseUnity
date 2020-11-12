9190ea8a18ea8956b3e2477046ae8e6a3e959ef1
Sumyakhuu <sumyahuu@gmail.com>
2020-11-12 13:42:32 +0800

    added spawn manager

---
+++ Assets/Scripts/spawnManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawnManager : MonoBehaviour
+{
+    class EnemySpawn
+    {
+        public float interval;
+        public float time;
+        public Vector3 poss;
+
+        public EnemySpawn(float aInterval, float aTime, Vector3 aPoss)
+        {
+            interval = aInterval;
+            time = aTime;
+            poss = aPoss;
+        }
+    }
+    public GameObject enemy;
+    EnemySpawn enemySpawn;
+
+    private void Start()
+    {
+        enemySpawn = new EnemySpawn(2f, 0f, new Vector3(-10f, 4, 0));
+    }
+    void Update()
+    {
+        spawnEnemy();
+    }
+
+    void spawnEnemy()
+    {
+        enemySpawn.time -= Time.deltaTime;
+        if (enemySpawn.time <= 0)
+        {
+            Instantiate(enemy, enemySpawn.poss, Quaternion.identity);
+            enemySpawn.time = enemySpawn.interval;
+        }
+    }
+}
--- Assets/Scripts/triggerDir.cs
+++ Assets/Scripts/triggerDir.cs
@@ -4,12 +4,35 @@ using UnityEngine;
 
 public class triggerDir : MonoBehaviour
 {
+    [SerializeField]
+    public enum Dir {up, down, right, left};
+
+
+    public Dir dir;
     private void OnTriggerEnter2D(Collider2D coll)
     {
 
         if (coll.tag == "Enemy")
         {
-            Debug.Log("GameObject2 collided with " + coll.name);
+            switch (dir)
+            {
+                case Dir.down:
+                    coll.gameObject.GetComponent<enemy>().movement = new Vector2(0f, -1f);
+                    break;
+                case Dir.up:
+                    coll.gameObject.GetComponent<enemy>().movement = new Vector2(0f, 1f);
+                    break;
+                case Dir.right:
+                    coll.gameObject.GetComponent<enemy>().movement = new Vector2(1f, 0f);
+                    break;
+                case Dir.left:
+                    coll.gameObject.GetComponent<enemy>().movement = new Vector2(-1f, 0f);
+                    break;
+                default:
+                    coll.gameObject.GetComponent<enemy>().movement = new Vector2(1f, 0f);
+                    break;
+            }
+            //Debug.Log("GameObject2 collided with " + coll.name);
         }
     }
 }
