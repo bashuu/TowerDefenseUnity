cc329d20c5dce2e5f40098b02a33b03fb913d3a6
Sumyakhuu <sumyahuu@gmail.com>
2020-11-13 21:33:55 +0800

    added tower.cs, changes in enemy.cs
    added tower.cs : state machine, tower attackspeed, tower trigger,
    changes in enemy.cs : added hp, destroyed after hp below 0;

--- Assets/Scripts/enemy.cs
+++ Assets/Scripts/enemy.cs
@@ -6,6 +6,7 @@ public class enemy : MonoBehaviour
 {
     public Vector2 movement;
     public float speed = 10f;
+    public int hp = 50;
     void Start()
     {
         movement = new Vector2(1.0f, 0.0f);
@@ -14,6 +15,12 @@ public class enemy : MonoBehaviour
     private void Update()
     {
         transform.Translate(movement * Time.deltaTime * speed);
+
+        if(hp <= 0)
+        {
+            Destroy(gameObject);
+        }
         //Debug.Log("testing");
     }
+
 }
---
+++ Assets/Scripts/tower.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tower : MonoBehaviour
+{
+    public GameObject enemy;
+    private float towerAttackSpeed = 0.5f;
+    private float attackSpeedCD = 0;
+    private int tmp;
+
+    public enum State
+    {
+        Idle,
+        Attack
+    }
+
+    public State state;
+
+    private void Start()
+    {
+        state = State.Idle;
+    }
+
+    private void OnTriggerEnter2D(Collider2D coll)
+    {
+        if(coll.tag == "Enemy")
+        { 
+            switch (state){
+                case State.Idle:
+                    enemy = coll.gameObject;
+                    state = State.Idle;
+                    break;
+                case State.Attack:
+                    break;
+                default:
+                    state = State.Idle;
+                    break;
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D coll)
+    {
+        if (coll.tag == "Enemy")
+        {
+            switch (state)
+            {
+                case State.Idle:
+                    break;
+                case State.Attack:
+                    if (enemy == coll.gameObject)
+                    {
+                        enemy = null;
+                        state = State.Idle;
+                    }
+                    break;
+                default:
+                    state = State.Idle;
+                    break;
+            }
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D coll)
+    {
+        if (coll.tag == "Enemy")
+        {
+            switch (state)
+            {
+                case State.Idle:
+                    enemy = coll.gameObject;
+                    state = State.Attack;
+                    break;
+                case State.Attack:
+                    break;
+                default:
+                    state = State.Idle;
+                    break;
+            }
+        }
+    }
+
+    private void Update()
+    {
+        if(enemy == null)
+        {
+            state = State.Idle;
+        }
+
+        switch (state)
+        {
+            case State.Idle:
+                break;
+            case State.Attack:
+                attackSpeedCD -= Time.deltaTime;
+                if (attackSpeedCD <= 0)
+                {
+                    enemy.GetComponent<enemy>().hp -= 5;
+                    attackSpeedCD = towerAttackSpeed;
+                }
+
+                break;
+            default:
+                state = State.Idle;
+                break;
+        }
+
+        Debug.Log(enemy);
+        /*Debug.Log(state);*/
+    }
+}
