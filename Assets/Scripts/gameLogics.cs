274d51fe89d4db01701cffaca709b13596ee5a7f
Sumyakhuu <sumyahuu@gmail.com>
2020-11-13 22:02:39 +0800

    added game logics, added build zone

---
+++ Assets/Scripts/gameLogics.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gameLogics : MonoBehaviour
+{
+    public int hitPoints = 5;
+
+    private void OnTriggerEnter2D(Collider2D coll)
+    {
+        if(coll.tag == "Enemy")
+        {
+            hitPoints--;
+            Destroy(coll.gameObject);
+        }
+    }
+}
